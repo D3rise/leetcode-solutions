4ad1b028207b237d37d4f00db688299abcb4fa03
D3rise <derise.dev@gmail.com>
2025-08-21 15:37:08 +0000

    Code challenge submission for question: 206 Reverse Linked List

---
+++ 206 Reverse Linked List/206reverse-linked-list.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prevNode *ListNode
+    currNode := head
+
+    for currNode != nil {
+        nextNode := currNode.Next
+        currNode.Next = prevNode
+        prevNode = currNode
+        currNode = nextNode
+    }
+
+    return prevNode
+}
