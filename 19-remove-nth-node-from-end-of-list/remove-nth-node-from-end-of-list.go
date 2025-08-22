026e5919fadf192c2f3ae91cc1c4ff88994d0eef
Vladimir Andrienko <derise2000@gmail.com>
2025-08-22 13:28:10 +0300

    Time: 0 ms (100.00%) | Memory: 4.1 MB (44.78%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -0,0 +1,19 @@
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    slow, fast := head, head
+    for i := 0; i < n; i++ {
+        fast = fast.Next
+    }
+
+    if fast == nil {
+        return head.Next
+    }
+
+    for fast.Next != nil {
+        fast = fast.Next
+        slow = slow.Next
+    }
+
+    slow.Next = slow.Next.Next
+
+    return head
+}
