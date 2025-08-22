d42637ddc4fc393bac6f4615a5d11586e0378218
Vladimir Andrienko <derise2000@gmail.com>
2025-08-22 15:25:12 +0300

    Time: 0 ms (100.00%) | Memory: 3.9 MB (86.21%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.go
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.go
@@ -12,12 +12,19 @@ func swapPairs(head *ListNode) *ListNode {
 
     current := dummy
     for current.Next != nil && current.Next.Next != nil {
+        // dummy -> 1 -> 2 -> 3
+        //          |    |
+        //        first  second
         first := current.Next
         second := current.Next.Next
 
+        // now:    2 -> 1 -> 3
+        //              ^
+        //      dummy is still pointing to 1
         first.Next = second.Next
         second.Next = first
 
+        // dummy -> 2 -> 1 -> 3
         current.Next = second
         current = first
     }
