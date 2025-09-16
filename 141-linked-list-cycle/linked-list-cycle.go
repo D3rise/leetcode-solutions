6521d003c24f170a754bb429de14a08d153ff00a
Vladimir Andrienko <derise2000@gmail.com>
2025-09-16 13:00:48 +0300

    Time: 7 ms (63.12%) | Memory: 6.2 MB (92.23%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.go
+++ 141-linked-list-cycle/linked-list-cycle.go
@@ -1,26 +1,17 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func hasCycle(head *ListNode) bool {
-    if head == nil {
+    if head == nil || head.Next == nil {
         return false
     }
 
-    slow := head
-    fast := head.Next
-
-    for fast != slow {
-        if fast == nil || fast.Next == nil {
-            return false
+    p1, p2 := head, head.Next
+    for p2.Next != nil && p2.Next.Next != nil {
+        if p1 == p2 {
+            return true
         }
 
-        slow = slow.Next
-        fast = fast.Next.Next
+        p1 = p1.Next
+        p2 = p2.Next.Next
     }
 
-    return true
+    return false
 }
